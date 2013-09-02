25a2a710f0e588059b7812c2fdfd0b32557f7af0
Sergey Belov <peimei@ya.ru>
2013-09-02 16:13:08 +0800

    Remove getTestLevelPath() function from bem-pr module

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ exports.extendMake = function(registry) {
     require('./nodes')(registry);
 };
 
-exports.getTestLevelPath = function() {
-    return exports.resolve('test.blocks');
-};
-
 exports.require = require;
 exports.resolve = PATH.resolve.bind(PATH, __dirname);
