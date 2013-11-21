3f67491133ac9feb9ba3517e004e139a0006dbd4
Vladimir Alaev <scff@yandex-team.ru>
2013-11-21 14:59:11 +0200

    Add missing bem.json.js tech

---
+++ techs/bem.json.js.js
@@ -0,0 +1,37 @@
+var PATH = require('path'),
+
+    environ = require('bem-environ'),
+
+    BEMJSON_ROOT = environ.getLibPath('bem-json', 'i-bem/__json'),
+    BEMJSON_CORE = PATH.join(BEMJSON_ROOT, 'i-bem__json.js');
+
+exports.baseTechName = 'js';
+
+exports.techMixin = {
+
+    getBuildSuffixesMap : function() {
+        return {
+            'bem.json.js': ['bemjson.js']
+        };
+    },
+
+    getBuildResult : function(files, suffix, output) {
+        var _this = this;
+        return this.__base.apply(this, arguments).then(function(res) {
+            res.unshift(_this.getBemjsonCore(output));
+            return res;
+        });
+    },
+
+    getBemjsonCore : function(outputDir) {
+        if(this.API_VER === 2) {
+            outputDir = PATH.dirname(outputDir);
+        }
+        return this.getBuildResultChunk(PATH.relative(outputDir, BEMJSON_CORE));
+    },
+
+    getDependencies : function() {
+        return ['bemdecl.js'];
+    }
+
+};
