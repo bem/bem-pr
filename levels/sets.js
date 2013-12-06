8abc502fec725310332f02b5d6f24749a6d69805
Vladimir Alaev <scff@yandex-team.ru>
2013-12-06 16:38:34 +0200

    Get rid of bem-core dependency, use blocks and techs from bem-bl@bem-xjst to build documentation pages (BEM-1522)

--- levels/sets.js
+++ levels/sets.js
@@ -13,8 +13,7 @@ exports.getTechs = function() {
         'js': 'v2/js-i',
         'css': 'v2/css',
         'bem.json.js': PATH.resolve(__dirname, '../techs/bem.json.js'),
-        'bemhtml': PATH.resolve(this.projectRoot, 'libs/bem-bl/blocks-common/i-bem/bem/techs/v2/bemhtml.js'),
-        'bemtree': PATH.resolve(this.projectRoot, 'libs/bem-core/.bem/techs/bemtree.js'),
-        'bemhtmlcore': PATH.resolve(this.projectRoot, 'libs/bem-core/.bem/techs/bemhtml.js')
+        'bemhtml': PATH.resolve(__dirname, '../libs/bem-bl/blocks-common/i-bem/bem/techs/v2/bemhtml.js'),
+        'bemtree': PATH.resolve(__dirname, '../libs/bem-bl/blocks-common/i-bem/bem/techs/v2/bemtree.js')
     };
 };
