e515680dca826fb239a5fddb840d970368d03176
Vladimir Alaev <scff@yandex-team.ru>
2013-12-24 16:17:00 +0200

    codestyle; use until.getBemTechPath() to resolve path to the level-proto tech

--- techs/examples.js
+++ techs/examples.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var U = require('bem/lib/util'),
-    PATH = require('path');
+var U = require('bem').util;
 
 exports.API_VER = 2;
 
-exports.baseTechPath = require.resolve('bem/lib/techs/v2/level-proto');
+exports.baseTechPath = U.getBemTechPath('level-proto', {version: 2});
 
 exports.techMixin = {
-
     getLevelProtoName: function() {
         return this.getTechName() + '-set';
     }
